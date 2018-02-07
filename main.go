903ed108878b1e83d69ea6af0d1ece7d882d0712
zaru@sakuraba <zarutofu@gmail.com>
2018-02-08 00:27:00 +0900

    Added command to modify the specified date

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"github.com/Songmu/prompter"
 	"github.com/olekukonko/tablewriter"
 	"github.com/urfave/cli"
+	"gopkg.in/AlecAivazis/survey.v1"
 )
 
 // Config is command parameters
@@ -85,6 +86,17 @@ func main() {
 			Usage: "Get your attendance list",
 			Action: func(c *cli.Context) error {
 				err := execGetAttendance()
+				if err != nil {
+					return cli.NewExitError(err, 1)
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "show",
+			Usage: "jobcan show [YYYYMMDD] / Show and fix time work for the specified day.",
+			Action: func(c *cli.Context) error {
+				err := execGetAttendanceByDay(c.Args().First())
 				if err != nil {
 					return cli.NewExitError(err, 1)
 				}
@@ -183,6 +195,179 @@ func execGetAttendance() error {
 	table.Render()
 
 	return nil
+}
+
+func execGetAttendanceByDay(day string) error {
+	var config Config
+
+	_, err := toml.DecodeFile(configPath(), &config)
+	if err != nil {
+		return fmt.Errorf("Config file is broken ;; please try `jobcan init`.")
+	}
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := &http.Client{Jar: jar}
+	login(client, config.Credential.ClientID, config.Credential.LoginID, config.Credential.Password)
+
+	r := regexp.MustCompile(`([0-9]{4})-?([0-9]{2})-?([0-9]{2})`)
+	result := r.FindAllStringSubmatch(day, -1)
+
+	res, err := client.Get(fmt.Sprintf("https://ssl.jobcan.jp/employee/adit/modify?year=%s&month=%s&day=%s",
+		result[0][1], result[0][2], result[0][3]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	table := tablewriter.NewWriter(os.Stdout)
+
+	// display column size
+	maxColumn := 4
+	// action column index number
+	actionColumnIndex := 5
+
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+
+	head := []string{}
+	doc.Find(".note tbody tr:first-child th").Each(func(i int, s *goquery.Selection) {
+		if i < maxColumn {
+			head = append(head, trimMetaChars(s.Text()))
+		}
+	})
+	table.SetHeader(head)
+
+	targetTimeLists := map[string]string{"cancel": "0"}
+	doc.Find(".note tbody tr").Each(func(i int, s *goquery.Selection) {
+		if i > 0 {
+			data := []string{}
+			s.Find("td").Each(func(i int, s *goquery.Selection) {
+				if i < maxColumn {
+					data = append(data, trimMetaChars(s.Text()))
+				} else if i == actionColumnIndex {
+					data, err := s.Find("a.btn-info").Attr("onclick")
+					if err != false {
+						r := regexp.MustCompile(`intoModifyMode\(([0-9]+), '([0-9]{2}:[0-9]{2})'`)
+						result := r.FindAllStringSubmatch(data, -1)
+						targetTimeLists[result[0][2]] = result[0][1]
+					}
+				}
+			})
+			table.Append(data)
+		}
+
+	})
+
+	table.Render()
+
+	fixFlag := promptFix()
+	if fixFlag == false {
+		return nil
+	}
+
+	targetTime := promptChooseTime(targetTimeLists)
+	if targetTime == "cancel" {
+		return nil
+	}
+
+	fixTime := promptFixTime()
+
+	params := formatFixTimeParams(doc)
+	params.DeleteMinute = targetTimeLists[targetTime]
+	params.Time = fixTime
+	sendFixTime(client, params)
+
+	return nil
+}
+
+func promptFix() bool {
+	fixFlag := false
+	prompt := &survey.Confirm{
+		Message: "Fix it?",
+	}
+	survey.AskOne(prompt, &fixFlag, nil)
+	return fixFlag
+}
+
+func promptChooseTime(targetTimeLists map[string]string) string {
+	targetTime := ""
+	keys := make([]string, 0, len(targetTimeLists))
+	for k := range targetTimeLists {
+		keys = append(keys, k)
+	}
+	prompt := &survey.Select{
+		Message: "Choose a time:",
+		Options: keys,
+	}
+	survey.AskOne(prompt, &targetTime, nil)
+	return targetTime
+}
+
+func promptFixTime() string {
+	time := ""
+	prompt := &survey.Input{
+		Message: "Input a time (HHMM)",
+	}
+	survey.AskOne(prompt, &time, nil)
+	return time
+}
+
+type FixTimeParams struct {
+	Token        string
+	DeleteMinute string
+	Time         string
+	GroupId      string
+	Notice       string
+	Year         string
+	Month        string
+	Day          string
+	ClientId     string
+	EmployeeId   string
+}
+
+func formatFixTimeParams(doc *goquery.Document) FixTimeParams {
+
+	token, _ := doc.Find("input[name=token]").Attr("value")
+	year, _ := doc.Find("input[name=year]").Attr("value")
+	month, _ := doc.Find("input[name=month]").Attr("value")
+	day, _ := doc.Find("input[name=day]").Attr("value")
+	clientId, _ := doc.Find("input[name=client_id]").Attr("value")
+	employeeId, _ := doc.Find("input[name=employee_id]").Attr("value")
+	groupId, _ := doc.Find("select[name=group_id] option:first-child").Attr("value")
+	return FixTimeParams{
+		Token:      token,
+		GroupId:    groupId,
+		Year:       year,
+		Month:      month,
+		Day:        day,
+		ClientId:   clientId,
+		EmployeeId: employeeId,
+	}
+}
+
+func sendFixTime(client *http.Client, params FixTimeParams) {
+	values := url.Values{}
+	values.Add("token", params.Token)
+	values.Add("delete_minutes", params.DeleteMinute)
+	values.Add("time", params.Time)
+	values.Add("group_id", params.GroupId)
+	values.Add("notice", "fix")
+	values.Add("year", params.Year)
+	values.Add("month", params.Month)
+	values.Add("day", params.Day)
+	values.Add("client_id", params.ClientId)
+	values.Add("employee_id", params.EmployeeId)
+	fmt.Println(values)
+	res, err := client.PostForm("https://ssl.jobcan.jp/employee/adit/insert/", values)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatal("Post error StatusCode=" + string(res.StatusCode))
+	}
 }
 
 func login(client *http.Client, clientID, loginID, password string) {
